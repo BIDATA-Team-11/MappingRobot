fab99f58a5e2ddc17b9c91bc43fbe4a5d29bd098
erlin <erlinssl@stud.ntnu.no>
2019-11-15 13:33:34 +0100

    Nesten litt javadoc

--- src/main/java/mappingrobot/LineChart.java
+++ src/main/java/mappingrobot/LineChart.java
@@ -1,13 +1,27 @@
 package mappingrobot;
-
+//TODO TORBJØRN HJELP
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+/**
+ *
+ *
+ * @author Stian Selvåg
+ * @author Herman Aagaard
+ * @author Henrik Hafsø
+ * @author Joakim Skogø Langvand
+ * @author Erling Sletta
+ * @author Torbjørn Øverås
+ * @author Gruppe 11, dataingeniør NTNU, første semester.
+ * @version 1.0.0
+ */
 public class LineChart extends Application {
-
+  /**
+   * Constructor that do the things that thing the dos
+   * @param args ???
+   */
   public LineChart(String[] args) {
     Application.launch(args);
   }
