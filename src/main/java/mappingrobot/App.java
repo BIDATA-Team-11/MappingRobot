c1fbe0515bcd387acf1a53168107232c8ed81cf7
AlphaStyle <torbjorn1991@hotmail.com>
2019-11-15 12:52:24 +0100

    Gjør OpenJFX til hoved thread

--- src/main/java/mappingrobot/App.java
+++ src/main/java/mappingrobot/App.java
@@ -2,12 +2,10 @@ package mappingrobot;
 
 import lejos.remote.ev3.RMIRemoteKey;
 import lejos.remote.ev3.RemoteEV3;
-//import lejos.remote.ev3.RMIRegulatedMotor;
 import java.rmi.RemoteException;
 import java.lang.InterruptedException;
 import java.io.IOException;
 import java.util.concurrent.LinkedBlockingQueue;
-//import java.util.HashMap;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -36,58 +34,46 @@ public class App extends Application {
     primaryStage.setTitle("MappingRobotChart");
     primaryStage.setScene(new Scene(root));
     primaryStage.show();
-  }
 
-  /**
-   * Main-method for the client. You get the choice to print out the colors
-   * the ColorSensor detects, calibrate the ColorSensor or make the robot drive.
-   *
-   * @param args Arguments will be ignored.
-   */
-  public static void main(String[] args) {
-    System.out.println("1.0.0-remotebot");
-
-    Application.launch(args);
     Thread current = new Thread();
 
     try {
-      // bot = new RemoteRobot();
-      // final RemoteEV3 ev3 = bot.getEV3();
       final RemoteEV3 ev3 = new RemoteEV3("10.0.1.1");
       ev3.setDefault();
 
       do {
         switch (ev3.getKeys().waitForAnyPress()) {
-        case RMIRemoteKey.RIGHT:
-          System.out.println("Right");
-          break;
+          case RMIRemoteKey.RIGHT:
+            System.out.println("Right");
+            break;
 
-        case RMIRemoteKey.LEFT:
-          System.out.println("Left");
+          case RMIRemoteKey.LEFT:
+            System.out.println("Left");
+            primaryStage.show();
 
-          current = new Thread(new Runnable() {
-            public void run() {
-              try {
-                start(ev3);
-              } catch (RemoteException e) {
-                System.out.println("Error: " + e.getMessage());
-                System.out.println("Here's the stacktrace. You figure it out.\n");
-                e.printStackTrace();
-              } catch (Exception e) {
-                System.out.println("Error: " + e.getMessage());
-                System.out.println("Here's the stacktrace. You figure it out.\n");
-                e.printStackTrace();
+            current = new Thread(new Runnable() {
+              public void run() {
+                try {
+                  start(ev3);
+                } catch (RemoteException e) {
+                  System.out.println("Error: " + e.getMessage());
+                  System.out.println("Here's the stacktrace. You figure it out.\n");
+                  e.printStackTrace();
+                } catch (Exception e) {
+                  System.out.println("Error: " + e.getMessage());
+                  System.out.println("Here's the stacktrace. You figure it out.\n");
+                  e.printStackTrace();
+                }
               }
-            }
-          });
+            });
 
-          current.start();
-          break;
+            current.start();
+            break;
 
-        case RMIRemoteKey.DOWN:
-          System.out.println("Down");
-          current.interrupt();
-          break;
+          case RMIRemoteKey.DOWN:
+            System.out.println("Down");
+            current.interrupt();
+            break;
         }
       } while (true);
     } catch (RemoteException e) {
@@ -99,6 +85,17 @@ public class App extends Application {
       System.out.println("Here's the stacktrace. You figure it out.\n");
       e.printStackTrace();
     }
+  }
+
+  /**
+   * Main-method for the client. You get the choice to print out the colors
+   * the ColorSensor detects, calibrate the ColorSensor or make the robot drive.
+   *
+   * @param args Arguments will be ignored.
+   */
+  public static void main(String[] args) {
+    System.out.println("1.0.0-remotebot");
+    Application.launch(args);
   }
 
   /**
@@ -256,18 +253,6 @@ public class App extends Application {
         }
       }
     });
-
-
-    // Thread chart = new Thread(new Runnable() {
-    //   public void run() {
-    //     try {
-    //       LineChart mapping = new LineChart();
-    //     } catch (Exception e) {
-    //       System.out.println(e);
-    //     }
-    //   }
-    // });
-
 
     // try {
       // radar.start();
