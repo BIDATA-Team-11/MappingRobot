0a1bee0c7fd94a6f7c08260c579de4a50f6933d5
Toukan-HTH <Henrik.T.H@Outlook.com>
2019-11-15 13:09:33 +0100

    done javadoc melding

--- src/main/java/mappingrobot/Melding.java
+++ src/main/java/mappingrobot/Melding.java
@@ -1,9 +1,22 @@
 package mappingrobot;
-
+/**
+ * @author Stian Selvåg
+ * @author Herman Aagaard
+ * @author Henrik Hafsø
+ * @author Joakim Skogål Langvand
+ * @author Erling Sletta
+ * @author Torbjørn Øverås
+ * @author Group 11, dataingeniør NTNU, første semester.
+ * @version 2.0.0
+ */
 public class Melding {
   public final Integer vinkel;
   public final Float distance;
-
+  /**
+   * Method sends a message to main
+   * @param vinkel
+   * @param distance
+   */
   public Melding(Integer vinkel, Float distance) {
     this.vinkel = vinkel;
     this.distance = distance;
